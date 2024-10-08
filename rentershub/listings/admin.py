00d1f2b60ec608c0c1f20fc252656bcd480f75e2
Eddie992 <ekataika2002@gmail.com>
2024-10-08 09:01:13 +0200

    admin.py models register

--- rentershub/listings/admin.py
+++ rentershub/listings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import PropertyPost, PropertyImages
 
 # Register your models here.
+admin.site.register(PropertyPost)
+admin.site.register(PropertyImages)
