af34ef1b141e0c06e001fc843d2e8c75b3b35963
Eddie992 <ekataika2002@gmail.com>
2024-10-08 09:30:35 +0200

    apps.py

--- rentershub/listings/apps.py
+++ rentershub/listings/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# This is the configuration file for the listings app
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'listings'
